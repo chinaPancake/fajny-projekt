4b4311c70608bb63fc238a06af223d424544dd6f
ChinaPancake <mateusz.placek99@gmail.com>
2024-03-02 17:37:23 +0100

    start of server side

---
+++ server/target/debug/deps/server.d
@@ -0,0 +1,5 @@
+C:\Users\sitle\dev\fajny_projekt\server\target\debug\deps\server.exe: src\main.rs
+
+C:\Users\sitle\dev\fajny_projekt\server\target\debug\deps\server.d: src\main.rs
+
+src\main.rs:
---
+++ server/target/debug/server.d
@@ -0,0 +1,1 @@
+C:\Users\sitle\dev\fajny_projekt\server\target\debug\server.exe: C:\Users\sitle\dev\fajny_projekt\server\src\main.rs
