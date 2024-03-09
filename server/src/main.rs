f7f7f728aad7845e07efec174982c8ed20b14518
ChinaPancake <mateusz.placek99@gmail.com>
2024-03-09 22:34:48 +0100

    bugged Message struct

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,9 @@
+use rocket::{
+    futures::channel::oneshot::channel,
+    serde::{Deserialize, Serialize},
+};
+use webrtc::mdns::message::Message;
+
 #[macro_use]
 extern crate rocket;
 
@@ -6,12 +12,20 @@ fn world() -> &'static str {
     "hello, world"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/hello", routes![world])
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+
+struct Message {
+    #[field(validate = len(..30))]
+    pub room: String,
+    #[field(validate = len(..20))]
+    pub username: String,
+    pub message: String,
 }
 
-#[get("/login")]
-fn login() -> &'static str {
-    "login"
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/hello", routes![world])
 }
